da9ab5ce9f88476a980afa4977aee34af19f7725
Vishwas Tamta <vishwastamta@gmail.com>
2021-12-13 23:55:18 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
+
 const App = () => {
+
+  let [month, day, year] = new Date().toLocaleString("en-US").split("/")
+  const [time, setTime] = useState(Date.now())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(Date.now())
+    },1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{day}/{month}/{year}</div>
     </div>
   )
 }
